e7257e60bcdb1345c194a32ab851332f5d297ea5
Jake Clarke <jake@theredfox.group>
2018-12-30 11:43:53 +1100

    hello world for leaflet awesome markers through node

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Initialize leaflet.js
 var L = require('leaflet');
+var Lam = require('leaflet.awesome-markers');
 
 // Initialize the map
 var map = L.map('map', {
-  scrollWheelZoom: false
+    scrollWheelZoom: false
 });
 
 // Set the position and zoom level of the map
@@ -11,6 +12,15 @@ map.setView([47.70, 13.35], 7);
 
 // Initialize the base layer
 var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    maxZoom: 19,
+    attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.marker([47.1, 13.34], {
+    icon: L.AwesomeMarkers.icon({
+        icon: 'spinner',
+        prefix: 'fa',
+        markerColor: 'red',
+        spin:true
+    })
 }).addTo(map);
